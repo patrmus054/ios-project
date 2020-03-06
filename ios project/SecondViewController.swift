a832ece5f14ec9f1f8aacc86ec8a8d40b749ed34
Patryk <patryk.musiol@sap.com>
2020-03-06 12:14:23 +0100

    Initial Commit

---
+++ ios project/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  ios project
+//
+//  Created by Musiol, Patryk on 06/03/2020.
+//  Copyright © 2020 Musiol, Patryk. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
